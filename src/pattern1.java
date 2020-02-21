94c9fc3aaf1e1c52d3c9982f95ad27e91675a29b
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-21 15:30:40 +0530

    initial commit taking input from the user

---
+++ src/pattern1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+    }
+}
