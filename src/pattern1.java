1bc05860249daff6a6308f6d40d510307b48a0d4
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-21 15:31:19 +0530

    final commit

--- src/pattern1.java
+++ src/pattern1.java
@@ -5,6 +5,16 @@ public class pattern1 {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
         int c = sc.nextInt();
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if (i == 1 || i == r || j == 1 || j == c) {
+                    System.out.print("1");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
